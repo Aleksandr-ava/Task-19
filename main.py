f451a375a979c45cdefe26a900c254f23c25ba7d
Aleksandr-ava <167477534+Aleksandr-ava@users.noreply.github.com>
2024-05-29 15:39:12 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+class Buiding:
+
+    total = 40
+
+    def __init__(self, total):
+        self.total = total
+
+    for total in range(1, 41):
+        print('Здание №', total)
+
+
+building = Buiding('Здание')
+
